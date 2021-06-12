5a39ce35d7b0bee1dce36dfa16e77fa7b87bf060
Thiago <thiagodepaulasouza@gmail.com>
2021-06-12 16:42:57 -0300

    refatoração - aplicando conceitos basicos de POO

---
+++ Receita/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Receita
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //componentes pra frita um ovo
+            ovo
+            frigideira
+            fogao
+            gas
+            oleo
+            uma espatula
+        }
+    }
+}
